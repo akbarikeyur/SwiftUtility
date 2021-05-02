e0a31adda242aaaeeb22273ae9003d5a57286d24
akbarikeyur <keyurdakbari@gmail.com>
2021-05-02 19:47:47 +0530

    App Update

---
+++ Sources/SwiftUtility/Common/MyClass.h
@@ -0,0 +1,16 @@
+//
+//  MyClass.h
+//  
+//
+//  Created by Keyur on 02/05/21.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MyClass : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
