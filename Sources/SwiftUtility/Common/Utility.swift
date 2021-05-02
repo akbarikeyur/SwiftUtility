0b21d738de883688d494055db0a35068918de0f9
akbarikeyur <keyurdakbari@gmail.com>
2021-05-02 19:50:21 +0530

    App Update

--- Sources/SwiftUtility/Common/Utility.swift
+++ Sources/SwiftUtility/Common/Utility.swift
@@ -434,3 +434,37 @@ extension UIApplication {
         return base
     }
 }
+
+public func checkForUpdate(_ completion: @escaping () -> Void) {
+    DispatchQueue.global(qos: .background).async {
+        do {
+            let update = try isUpdateAvailable()
+            print("update",update)
+            if update{
+                completion()
+                return
+            }
+        } catch {
+            print(error)
+        }
+    }
+}
+
+public func isUpdateAvailable() throws -> Bool {
+    guard let info = Bundle.main.infoDictionary,
+        let currentVersion = info["CFBundleShortVersionString"] as? String,
+        let identifier = info["CFBundleIdentifier"] as? String,
+        let url = URL(string: "http://itunes.apple.com/lookup?bundleId=\(identifier)") else {
+            throw VersionError.invalidBundleInfo
+    }
+    let data = try Data(contentsOf: url)
+    guard let json = try JSONSerialization.jsonObject(with: data, options: [.allowFragments]) as? [String: Any] else {
+        throw VersionError.invalidResponse
+    }
+    if let result = (json["results"] as? [Any])?.first as? [String: Any], let version = result["version"] as? String {
+        print("version in app store", version,currentVersion);
+        
+        return Float(version)! > Float(currentVersion)!
+    }
+    throw VersionError.invalidResponse
+}
